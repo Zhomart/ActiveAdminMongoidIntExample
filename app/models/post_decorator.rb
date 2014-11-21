b688e035e88626b9ad4ee3da3deebde74d5d1d07
Zhomart Mukhamejanov <mzhomart@gmail.com>
2014-11-21 12:20:28 -0800

    post_decorator disabled

--- app/models/post_decorator.rb
+++ app/models/post_decorator.rb
@@ -1,11 +1,11 @@
-require 'draper'
+# require 'draper'
 
-class PostDecorator < Draper::Decorator
-  decorates :post
-  delegate_all
+# class PostDecorator < Draper::Decorator
+#   decorates :post
+#   delegate_all
 
-  def decorator_method
-    'A method only available on the decorator'
-  end
-end
+#   def decorator_method
+#     'A method only available on the decorator'
+#   end
+# end
 
